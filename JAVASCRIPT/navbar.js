a6ab19242f9cafce9ce32855cd6987346c6fba6f
Mathias Gumpen Gundersen <Mathias.g.gundersen@live.no>
2023-05-28 20:28:37 +0200

    site

--- JAVASCRIPT/navbar.js
+++ JAVASCRIPT/navbar.js
@@ -1,10 +1,6 @@
-`
-<div class="hamburger-container" data-state="open">
-				<div class="bar"></div>
-				<div class="bar"></div>
-				<div class="bar"></div>
-			</div>
-`;
+import client from './shared/builder.js';
+import builder from './shared/urlMaker.js';
+const urlFor = (source) => builder.image(source);
 
 const sideBarState = {
 	open: false,
@@ -13,6 +9,58 @@ const sideBarState = {
 const hamburgerContainer = document.querySelector('.hamburger-container');
 const modal = document.querySelector('.navbar-modal');
 const backdrop = document.querySelector('.navbar-backdrop');
+const searchbar = document.querySelector('.navbar-search');
+const searchDropdown = document.querySelector('.search-dropdown');
+
+function debounce(func, timeout = 300) {
+	let timer;
+	return (...args) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			func.apply(this, args);
+		}, timeout);
+	};
+}
+
+const processChange = debounce((e) => queryForPosts(e));
+
+const queryForPosts = async (e) => {
+	console.log(e.target.value.length);
+	if (e.target.value.length === 0) {
+		return;
+	}
+	const data = await client.fetch(
+		`*[_type == 'post' && [title] match '${e.target.value}']`
+	);
+
+	showSearchDropdown(data);
+};
+
+const showSearchDropdown = (data) => {
+	searchDropdown.innerHTML = '';
+	searchDropdown.dataset.state = 'open';
+
+	data.forEach((element) => {
+		const a = document.createElement('a');
+		a.href = `post.html?slug=${element.slug.current}`;
+		a.classList.add('search-dropdown-item');
+		const img = document.createElement('img');
+		img.src = urlFor(element.mainImage.asset._ref);
+		const h2 = document.createElement('h2');
+		h2.innerText = element.title;
+		a.append(img);
+		a.append(h2);
+		searchDropdown?.append(a);
+	});
+};
+
+searchbar?.addEventListener('keydown', (e) => {
+	if (e.target.value.length === 1) {
+		searchDropdown.dataset.state = 'closed';
+		return;
+	}
+	processChange(e);
+});
 
 hamburgerContainer?.addEventListener('click', (e) => {
 	console.log('click');
@@ -20,11 +68,14 @@ hamburgerContainer?.addEventListener('click', (e) => {
 });
 
 backdrop?.addEventListener('click', (e) => {
-	console.log('backdrop');
+	backdrop.dataset.state = 'closed';
+	searchDropdown.dataset.state = 'closed';
+
 	sideBarState.open = false;
 });
 
 window.addEventListener('click', () => {
+	searchDropdown.dataset.state = 'closed';
 	sideBarState.open ? disableScroll() : enableScroll();
 
 	sideBarState.open
